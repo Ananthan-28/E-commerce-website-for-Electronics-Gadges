8b16eb5257eba6f32773ad03ddbbdfa2935c0721
Ananthan-28 <ananthanr08@gmail.com>
2024-04-23 16:07:46 +0530

    Initial commit

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'E_commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': '****************',
         'NAME': 'project_techbuy',
         'USER':'root',
         'PASSWORD':'',
@@ -148,6 +148,6 @@ SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'techbuy97@gmail.com'
+EMAIL_HOST_USER = '**********@gmail.com'
 EMAIL_HOST_PASSWORD = 'fcucayofbvdowqyh'
 EMAIL_USE_TLS = True
