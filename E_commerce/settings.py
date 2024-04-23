7717d6bfa7e2b6fe12f8bf9d97eab6e29951f68f
Ananthan-28 <ananthanr08@gmail.com>
2024-04-23 20:17:35 +0530

    changing sensitive contents (gmail)

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -148,6 +148,6 @@ SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '******@gmail.com'
+EMAIL_HOST_USER = '***@gmail.com'
 EMAIL_HOST_PASSWORD = 'fcucayofbvdowqyh'
 EMAIL_USE_TLS = True
